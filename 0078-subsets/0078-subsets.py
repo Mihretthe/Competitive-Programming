5da51369a4e6f0a30320b1770e134233e4c22272
Mihretthe <115806991+Mihretthe@users.noreply.github.com>
2023-03-28 00:09:59 +0300

    Time: 32 ms (81.65%), Space: 13.9 MB (99.55%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -3,8 +3,7 @@ class Solution:
         a=[]
         for i in range(1,len(nums)):
             comb=combinations(nums,i)
-            l=list((comb))
-            a+=l
+            a+=list((comb))
         a.append([])
         a.append(nums)
         return a
