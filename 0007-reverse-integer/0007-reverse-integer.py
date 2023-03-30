e8357915ce3c0d7531a524859e4b997c503118ef
Mihretthe <115806991+Mihretthe@users.noreply.github.com>
2023-03-30 22:24:57 +0300

    Time: 36 ms (48.64%), Space: 13.8 MB (50.68%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -7,12 +7,7 @@ class Solution:
             s=s[:-1]
         x=int(s)
         a=bin(x)[2:]
-        print(len(a))
-        # print(a[::-1])
-        # a=a[::-1]
         if len(a)>=32 and x>0 or x<0 and len(a)>32:
             return 0
-        # elif a>32:
-        #     return 0
         else:
             return x
