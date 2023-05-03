90022963c9130b2f8900735ffd25bc4ca656048e
Mihretthe <115806991+Mihretthe@users.noreply.github.com>
2023-05-03 22:32:25 +0300

    Time: 62 ms (5.89%), Space: 16.2 MB (5.03%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -6,10 +6,8 @@ class Solution:
         for i in range(len(nums)):
             for j in range(len(nums)):
                 if(nums[i]<nums[j]):
-                    temp=nums[i]
-                    nums[i]=nums[j]
-                    nums[j]=temp
-        
+                    nums[i],nums[j]=nums[j],nums[i]
+                    
         
         
         
