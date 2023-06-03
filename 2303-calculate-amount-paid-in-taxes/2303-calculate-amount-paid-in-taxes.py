5c4572ffc0d7d32a0d618921e7cdd7d8451255c8
Mihretthe <115806991+Mihretthe@users.noreply.github.com>
2023-06-03 09:24:03 +0300

    Time: 96 ms (14.78%), Space: 16.3 MB (46.23%) - LeetHub

--- 2303-calculate-amount-paid-in-taxes/2303-calculate-amount-paid-in-taxes.py
+++ 2303-calculate-amount-paid-in-taxes/2303-calculate-amount-paid-in-taxes.py
@@ -1,11 +1,29 @@
 class Solution:
     def calculateTax(self, brackets: List[List[int]], income: int) -> float:
-        tax = 0
-        lower_bound = 0
-        for upper_bound, rate in brackets:
-            if income <= lower_bound:
+        """
+        brackets - array
+        for each brackets there is an upper and its persent
+        brackets are already sorted by upper bound
+        the first upper times the persent plus the others like the two consecutive differences
+        times there persent each
+        there is another given integer income reperesenting the amount of money you earned 
+        the income is to bound the upper the person can not earn more than its income
+        
+        """
+        tax=0
+        idx=0
+        for i in range(len(brackets)):
+            if brackets[i][0]>=income:
+                idx=i
                 break
-            taxable_income = min(income - lower_bound, upper_bound - lower_bound)
-            tax += taxable_income * rate / 100
-            lower_bound = upper_bound
+        brackets[idx][0]=income  
+        brackets=brackets[:idx+1]
+        l=len(brackets)-1
+        while l>0:
+            tax+=(brackets[l][0]-brackets[l-1][0])*(brackets[l][1]/100)
+            l-=1
+        tax+=brackets[0][0]*brackets[0][1]/100
         return tax
+            
+       
+        
