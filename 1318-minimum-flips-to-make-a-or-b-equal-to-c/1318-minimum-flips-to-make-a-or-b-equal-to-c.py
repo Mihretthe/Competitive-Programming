429bc633d90d36399ca073348d141cc49397bf8a
Mihretthe <115806991+Mihretthe@users.noreply.github.com>
2023-06-07 12:25:58 +0300

    Time: 45 ms (53.19%), Space: 16.4 MB (25.27%) - LeetHub

---
+++ 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.py
@@ -0,0 +1,27 @@
+class Solution:
+    def minFlips(self, a: int, b: int, c: int) -> int:
+        a=format(a,"04b")
+        b=format(b,"04b")
+        c=format(c,"04b")
+        n=max(len(a),len(b),len(c))
+        s="0"*(n-len(a))
+        a=s+a
+        s="0"*(n-len(c))
+        c=s+c
+        s="0"*(n-len(b))
+        b=s+b
+        count=0
+        print("a b c count")
+        for i in range(n):
+            if a[i]==b[i]==c[i] or a[i]=="1" and b[i]=="0" and c[i]=="1" or a[i]=="0" and b[i]=="1" and c[i]=="1":
+                continue
+            elif a[i]==b[i]=="1" and c[i]=="0":
+                count+=2
+            elif a[i]==b[i]=="0" and c[i]=="1":
+                count+=1
+            elif  a[i]=="1" and b[i]=="0" or a[i]=="0" and b[i]=="1" and c[i]=="0":
+                count+=1
+            else:
+                count+=1
+            print(a[i],b[i],c[i],count)
+        return count
