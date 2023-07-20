fa1f69a134d719c7124afdc16f1015cfe9f5191c
Mihretthe <115806991+Mihretthe@users.noreply.github.com>
2023-07-21 01:58:08 +0300

    Time: 5357 ms (5.00%), Space: 77.5 MB (80.10%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.py
@@ -0,0 +1,33 @@
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.store = {}
+        self.recently = []
+        self.capacity = capacity
+
+    def get(self, key: int) -> int:
+        if key in self.store.keys():
+            if key in self.recently:
+                self.recently.remove(key)
+            self.recently.append(key)
+            return self.store[key]
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.store.keys():
+            self.store[key] = value
+        elif key not in self.store.keys() and len(self.store) < self.capacity:
+            self.store[key] = value
+        elif key not in self.store.keys() and len(self.store) >= self.capacity:
+            del self.store[self.recently[0]]
+            self.recently = self.recently[1:]
+            self.store[key] = value
+        if key in self.recently:
+            self.recently.remove(key)
+        self.recently.append(key)
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
