cf2c9758d899ae914e92d53824d7828c1bfcb5fb
Mihretthe <115806991+Mihretthe@users.noreply.github.com>
2023-09-20 17:11:38 +0300

    Time: 911 ms (97.85%), Space: 30.3 MB (83.06%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.py
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.py
@@ -1,28 +1,26 @@
 class Solution:
-    def minOperations(self, nums: List[int], x: int) -> int:
-    
-        totalSum = sum(nums)
-        targetSum = totalSum - x
+    def minOperations(self, nums: List[int], x: int) -> int:        
+        t = sum(nums) - x
 
-        if targetSum == 0:
+        if t == 0:
             return len(nums)
 
         n = len(nums)
-        left = 0
-        currentSum = 0
-        minOperations = float('inf')
+        l = 0
+        c = 0
+        m = float('inf')
 
-        for right in range(n):
-            currentSum += nums[right]
+        for r in range(n):
+            c += nums[r]
 
-            while currentSum > targetSum and left <= right:
-                currentSum -= nums[left]
-                left += 1
+            while c > t and l <= r:
+                c -= nums[l]
+                l += 1
 
-            if currentSum == targetSum:
-                minOperations = min(minOperations, n - (right - left + 1))
+            if c == t:
+                m = min(m, n - (r - l + 1))
 
-        if minOperations == float('inf'):
+        if m == float('inf'):
             return -1
         else:
-            return minOperations
+            return m 
