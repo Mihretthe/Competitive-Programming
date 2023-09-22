8d945741cb80d9b94508ac31209f5b3755da2b55
Mihretthe <115806991+Mihretthe@users.noreply.github.com>
2023-09-22 16:32:50 +0300

    Time: 41 ms (61.89%), Space: 16.5 MB (83.72%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,18 +1,12 @@
 class Solution:
-    def generateParenthesis(self, n: int) -> List[str]:
-        
-        
-        def backtrack(n):
-            if n == 0:
-                return [""]
-            parenthesis = []
-            
-            for i in range(n):
-                
-                inner = backtrack(i)
-                outer = backtrack(n-i-1)
-                for j in inner:
-                    for k in outer:
-                        parenthesis.append(f'({j}){k}')
-            return parenthesis
-        return backtrack(n)
+    def generateParenthesis(self, n: int) -> List[str]:       
+        if n == 0:
+            return [""]
+        parenthesis = []
+        for i in range(n):
+            inner = self.generateParenthesis(i)
+            outer = self.generateParenthesis(n-i-1)
+            for j in inner:
+                for k in outer:
+                    parenthesis.append(f'({j}){k}')
+        return parenthesis
