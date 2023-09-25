916a8b8e6f94f90c1e7d8c80344747fc8369eec6
Mihretthe <115806991+Mihretthe@users.noreply.github.com>
2023-09-25 22:09:27 +0300

    Time: 1661 ms (59.94%), Space: 18.1 MB (41.96%) - LeetHub

--- 2514-count-anagrams/2514-count-anagrams.py
+++ 2514-count-anagrams/2514-count-anagrams.py
@@ -4,14 +4,12 @@ class Solution:
         ans = 1
         for i in s:
             a = Counter(i)
-            if len(a) == 1:
-                ans *= 1
-            else: 
-                c = 1
-                for j in a:
-                    if a[j] > 1:
-                        c *= math.factorial(a[j])
-                ans *= (math.factorial(len(i)) // c)
+            
+            c = 1
+            for j in a:
+                if a[j] > 1:
+                    c *= math.factorial(a[j])
+            ans *= (math.factorial(len(i)) // c)
         return ans % (10**9 + 7)
                 
             
