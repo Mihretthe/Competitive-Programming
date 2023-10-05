02e7a4ddf43cc12e73f7e9eef84374bf8680a4bb
Mihretthe <115806991+Mihretthe@users.noreply.github.com>
2023-10-05 21:48:03 +0300

    Time: 2626 ms (5.17%), Space: 424.3 MB (5.44%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,11 +1,23 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        def nooverlap(lists):
+            lists.sort()
+            for i in range(len(lists) - 1):
+                if lists[i][1] in range(lists[i + 1][0],lists[i + 1][1] + 1):
+                    return i
+                elif lists[i + 1][0] in range(lists[i ][0],lists[i ][1] + 1):
+                    return i
+            return -1
+        def mergeing(a,b):
+            return [a[0],max(a[1],b[1])]
         intervals.sort()
-        o=[intervals[0]]
-        for i, j in intervals[1:]:
-            last=o[-1][1]
-            if i<=last:
-                o[-1][1]=max(last,j)
-            else:
-                o.append([i,j])
-        return o
+        idx = nooverlap(intervals)
+        if idx == -1:
+            return intervals
+        else:
+            intervals[idx] = mergeing(intervals[idx],intervals[idx + 1])
+            intervals = intervals[:idx + 1] + intervals[idx + 2:]
+            return self.merge(intervals)
+                
+                
+        return []
