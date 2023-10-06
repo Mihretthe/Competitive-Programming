99f1b18b997fd19f18fbffc85abb9990c5f25905
Mihretthe <115806991+Mihretthe@users.noreply.github.com>
2023-10-06 16:34:51 +0300

    Update 0075-sort-colors.py

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,12 +1,27 @@
+# class Solution:
+#     def sortColors(self, nums: List[int]) -> None:
+#         """
+#         Do not return anything, modify nums in-place instead.
+        """
+        # for i in range(len(nums)):
+        #     for j in range(len(nums)):
+        #         if(nums[i]<nums[j]):
+        #             nums[i],nums[j]=nums[j],nums[i]
+
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
+        counter = [0] * 3
+        for i in nums:
+            counter[i] += 1
+        new_arr = []
+        
+        for i in range(3):
+            new_arr += ([i] * counter[i]) 
         for i in range(len(nums)):
-            for j in range(len(nums)):
-                if(nums[i]<nums[j]):
-                    nums[i],nums[j]=nums[j],nums[i]
+            nums[i] = new_arr[i]
                     
         
         
