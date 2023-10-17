c1c85ae27a62b3514b6d1df2522a895652b1de4e
Mihretthe <115806991+Mihretthe@users.noreply.github.com>
2023-10-17 11:24:51 +0300

    Time: 29 ms (96.67%), Space: 16.3 MB (52.78%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -4,14 +4,14 @@ class Solution:
             
             for i in range(n + 1):
                 if i > 0:
-                    b = ans[-1]
+                    
                     a = [0]
-                    for j in range(1,len(b)):
-                        a.append(b[j] + b[j - 1])
+                    for j in range(1,len(ans)):
+                        a.append(ans[j] + ans[j - 1])
                     a += [0]
                 else:
                     a = [0,1,0]
-                ans.append(a)
-            return ans[-1][1:-1]
+                ans = a
+            return ans[1:-1]
         return triangle(n, [])
                     
