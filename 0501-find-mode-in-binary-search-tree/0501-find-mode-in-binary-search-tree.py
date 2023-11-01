c35b8aa97b2aa828034e54005b8e6e6a5a43f395
Mihretthe <115806991+Mihretthe@users.noreply.github.com>
2023-11-01 11:19:13 +0300

    Time: 59 ms (41.32%), Space: 21.2 MB (6.55%) - LeetHub

--- 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.py
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.py
@@ -6,15 +6,17 @@
 #         self.right = right
 class Solution:
     def findMode(self, root: Optional[TreeNode]) -> List[int]:
-        def trav(result, root):
+        def trav(root, ans):
             if root:
-                result.append(root.val)
-                trav(result, root.left)      
-                trav(result, root.right)
-            return result
-        result = trav([], root)
-        resultSet = set(result)
-        max_count = 1
-        for i in resultSet:
-            max_count = max(max_count, result.count(i))
-        return [i for i in resultSet if result.count(i) == max_count]
+                ans.append(root.val)
+                trav(root.left,ans)    
+                trav(root.right,ans)
+            return ans
+        trav = trav(root, [])
+        ans = []
+        counter = Counter(trav)
+        m = max(list(counter.values()))
+        for i in set(trav):
+            if counter[i] == m:
+                ans.append(i)
+        return ans
