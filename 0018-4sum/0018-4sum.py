a0dc858fd7470ea39fd0235f2fb0b60505d3701d
Mihretthe <115806991+Mihretthe@users.noreply.github.com>
2023-11-17 06:22:08 +0300

    Time: 1220 ms (16.84%), Space: 16.4 MB (43.84%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -1,23 +1,23 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-        ans = []
-        
         nums.sort()
-        
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                l, r = j + 1, len(nums) - 1
-                remaining = target - (nums[i] + nums[j])
-                
+        ans = []
+        for i in range(len(nums)):    
+            for j in range(i + 1,len(nums)):
+                l = j + 1
+                r = len(nums) - 1
+                s = target - nums[i] - nums[j]
                 while l < r:
-                    if nums[l] + nums[r] == remaining:
-                        if sorted([nums[i], nums[j], nums[l], nums[r]]) not in ans:
-                            ans.append(sorted([nums[i], nums[j], nums[l], nums[r]]))
-                        l += 1
-                    elif nums[l] + nums[r] < remaining:
-                        l += 1
-                    elif nums[l] + nums[r] > remaining:
+                    if nums[l] + nums[r] == s:
+                        if [nums[i],nums[j], nums[l],nums[r]] not in ans:
+                            ans.append([nums[i],nums[j], nums[l],nums[r]])
                         r -= 1
-                        
+                    elif nums[l] + nums[r] < s: 
+                        l += 1
+                    elif nums[l] + nums[r] > s:
+                        r -= 1
         return ans
-                
+
+                    
+
+        
