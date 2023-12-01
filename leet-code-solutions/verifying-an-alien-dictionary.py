9e0c85ff20be726e4bf3670596d14f9fa753bf3e
Mihretthe <115806991+Mihretthe@users.noreply.github.com>
2023-12-01 19:49:04 +0300

    Added solution for Verifying an Alien Dictionary

--- leet-code-solutions/verifying-an-alien-dictionary.py
+++ leet-code-solutions/verifying-an-alien-dictionary.py
@@ -1,16 +1,22 @@
 class Solution:
     def isAlienSorted(self, words: List[str], order: str) -> bool:
+        hashmap = {}
+        for i in range(len(order)):
+            hashmap[order[i]] = i
+        
+
+
+
         for i in range(len(words) - 1):
             a = words[i]
             b = words[i + 1]
             m = min(len(a), len(b))
             l = 0
+
             while l < m:
-                if order.index(a[l]) > order.index(b[l]) and a[:l] == b[:l]:
-                    print(a[l], b[l])
+                if hashmap[a[l]] > hashmap[b[l]] and a[:l] == b[:l]:
                     return False
                 l += 1
-            print(l,len(b) - 1,len(a) - l)
             if  len(a) - l > 0 and len(b) - l == 0 and a[:l] == b[:l]:
                 return False
             
