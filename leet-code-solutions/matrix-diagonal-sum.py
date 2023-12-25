7d6292b6fe05ab13912160a9486924e93635841c
Mihretthe <115806991+Mihretthe@users.noreply.github.com>
2023-12-26 00:15:44 +0300

    Added solution for Matrix Diagonal Sum

--- leet-code-solutions/matrix-diagonal-sum.py
+++ leet-code-solutions/matrix-diagonal-sum.py
@@ -4,15 +4,11 @@ class Solution:
         diagonal_sum = 0
 
         for i in range(n):
-            diagonal_sum += mat[i][i] + mat[i][n - 1 - i]     
+            diagonal_sum += mat[i][i]
+            diagonal_sum += mat[i][n - 1 - i]
 
         if n % 2 == 1:
             diagonal_sum -= mat[n//2][n//2]
 
         return diagonal_sum
 
-
-
-
-
-        
