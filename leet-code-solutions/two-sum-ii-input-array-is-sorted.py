92ba4697474fbd1b1e99b1123b577f8badb0317c
Mihretthe <115806991+Mihretthe@users.noreply.github.com>
2024-01-02 18:30:33 +0300

    Added solution for Two Sum II - Input Array Is Sorted

---
+++ leet-code-solutions/two-sum-ii-input-array-is-sorted.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        l = 0
+        r = len(nums) - 1
+
+        while l < r:
+            if nums[l] + nums[r] == target:
+                return [l + 1, r + 1]
+            elif nums[l] + nums[r] < target:
+                l += 1
+            else:
+                r -= 1
