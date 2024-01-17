f6f2920112d7efc76b0ea3dd45b1b463e45ad9ab
Mihretthe <115806991+Mihretthe@users.noreply.github.com>
2024-01-17 11:16:05 +0300

    Added solution for Permutation in String

--- leet-code-solutions/permutation-in-string.py
+++ leet-code-solutions/permutation-in-string.py
@@ -1,9 +1,22 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        min_length = len(s1)
-        counter = Counter(s1)
+        k = len(s1)
+        length_2 = len(s2)
+        counter_1 = Counter(s1)
+        counter = Counter(s2[:k])
+        left = 0
+        if counter_1 == counter:
+            return True
 
-        for i in range(len(s2)):
-            if Counter(s2[i: i + min_length]) == counter:
+        for right in range(k,length_2):
+            counter[s2[left]] -= 1
+            if counter[s2[left]] == 0:
+                del counter[s2[left]]
+            counter[s2[right]] += 1
+            if counter == counter_1:
                 return True
+            left += 1
+
         return False
+
+
