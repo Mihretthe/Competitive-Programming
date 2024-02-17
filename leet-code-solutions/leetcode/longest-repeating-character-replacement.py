7fa260720fb5a3977b2600e47c517067b7cf84a4
Mihretthe <115806991+Mihretthe@users.noreply.github.com>
2024-02-17 20:26:02 +0300

    Add solution for Longest Repeating Character Replacement

--- leet-code-solutions/leetcode/longest-repeating-character-replacement.py
+++ leet-code-solutions/leetcode/longest-repeating-character-replacement.py
@@ -1,34 +1,20 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        def makeMax(counter):
-            if not counter:
-                return ""
-            max_count = ""
-            val = 0
-            for key, value in counter.items():
-                if value > val:
-                    max_count = key
-                    val = value
-            
-            return max_count
-
+        # only the maximum frequency makes change
         length = len(s)
         counter = defaultdict(int)
         left = 0
         longest = 0
-        max_count = makeMax(counter)
+        max_count = 0
 
         for right in range(length):
             counter[s[right]] += 1
-            max_count = makeMax(counter)
+            max_count = max(max_count, counter[s[right]])
             window = right - left + 1
-            while left < right and window - counter[max_count] > k:             
+            while left < right and window - max_count > k:             
                 counter[s[left]] -= 1
-                window -= 1
-                max_count = makeMax(counter)                
+                window -= 1              
                 left += 1
-            longest = max(longest, right - left + 1)      
-
-                
+            longest = max(longest, right - left + 1)                      
         return longest
         
