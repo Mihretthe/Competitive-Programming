5da9272f756d931420c218fef4a40d40a74d04d2
Mihret Tekalgn <115806991+Mihretthe@users.noreply.github.com>
2024-10-17 23:51:48 +0300

    Time: 0 ms (100%), Space: 16.6 MB (42.71%) - LeetHub

---
+++ 0670-maximum-swap/0670-maximum-swap.py
@@ -0,0 +1,27 @@
+class Solution:
+    def maximumSwap(self, num: int) -> int:
+        num_str = str(num)
+        n = len(num_str)
+        last_seen = [-1] * 10  # Store the last occurrence of each digit
+
+        # Record the last occurrence of each digit
+        for i in range(n):
+            last_seen[int(num_str[i])] = i
+
+        # Traverse the string to find the first digit that can be swapped with a larger one
+        for i in range(n):
+            for d in range(9, int(num_str[i]), -1):
+                if last_seen[d] > i:
+                    # Perform the swap
+                    num_str = list(num_str)
+                    num_str[i], num_str[last_seen[d]] = (
+                        num_str[last_seen[d]],
+                        num_str[i],
+                    )
+                    num_str = "".join(num_str)
+
+                    return int(
+                        num_str
+                    )  # Return the new number immediately after the swap
+
+        return num  # Return the original number if no swap can maximize it
