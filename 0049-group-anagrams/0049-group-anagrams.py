5ead0e60266dce73465c72b1ac0c5bf9670aeaa6
Mihret Tekalgn <115806991+Mihretthe@users.noreply.github.com>
2024-12-24 23:27:19 +0300

    Time: 9 ms (88.23%), Space: 20.6 MB (44.17%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,27 +1,9 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        
-#         ans = []
-#         v = []
-#         for i in range(len(strs)):
-#             if strs[i] in v:
-#                 continue
-#             a = Counter(strs[i])
-#             ana = []
-#             for j in strs:
-#                 if Counter(j) == a and j not in v:
-#                     ana.append(j)
-#             v += ana
-#             ans.append(ana)
-                    
-        
-#         return ans    
-            
-        memo = defaultdict(list)
-        for i in strs:
-            s = "".join(sorted(i))
-            memo[s].append(i)
-        return memo.values()
-        
-        
-                    
+
+        anagrams = defaultdict(list)
+
+        for str in strs:
+            anagrams["".join(sorted(str))].append(str)
+
+        return list(anagrams.values())
