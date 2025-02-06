ae4d3df4ede02d46bdca95b21fe1f5e0fe41541d
Mihret Tekalgn <115806991+Mihretthe@users.noreply.github.com>
2025-02-07 00:11:21 +0300

    Time: 0 ms (100%), Space: 17.9 MB (34.22%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -1,66 +1,44 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        first_row = 0
-        last_row = len(matrix) - 1
-        last_column = len(matrix[0]) - 1
-        first_column = 0
+        rows = len(matrix)
+        cols = len(matrix[0])
+        top = 0 
+        bottom = rows - 1
+        left = 0
+        right = cols - 1
 
-        spiral = []
-        visit = set([(0,i) for i in range(len(matrix[0]))])
+        answer = []
 
-        r = first_row
-        l = last_column
-
-        spiral += matrix[r]
-        r += 1
-        for _ in range(len(matrix[0])):
-            print(spiral,r,l)
+        while top <= bottom and left <= right:
             
-            while r <= last_row:
-                if (r,l) not in visit:
-                    spiral.append(matrix[r][l])
-                    visit.add((r,l))
-                r += 1
+            j = left
+            while j <= right and len(answer) < rows * cols:
+                answer.append(matrix[top][j])
+                j += 1
             
-            first_row += 1
+            top += 1
             
-            r -= 1
-            last_column -= 1
-            l -= 1 
-            while l >= first_column:
-                if (r,l) not in visit:
-                    spiral.append(matrix[r][l])
-                    visit.add((r,l))
-                l -= 1
-                    
-            l += 1
-            last_row -= 1
+            j = top
+            while j <= bottom and len(answer) < rows * cols:
+                answer.append(matrix[j][right])
+                j += 1   
             
 
-            while r > first_row:
-                r -= 1
-                if (r,l) not in visit:
-                    spiral.append(matrix[r][l])
-                    visit.add((r,l))
-
-
-             
-            while l < last_column:
-                l += 1
-                if (r,l) not in visit:
-                    spiral.append(matrix[r][l])
-                    visit.add((r,l))
-            first_column += 1   
-
+            right -= 1
             
-            while r < last_row:
-                r += 1
-                if (r,l) not in visit:
-                    spiral.append(matrix[r][l])
-                    visit.add((r,l))
             
-            print(spiral,r,l)
-            if len(spiral) >= (len(matrix) * len(matrix[0])):
-              return spiral
-        return spiral
+            j = right
+            while j >= left and len(answer) < rows * cols:
+                answer.append(matrix[bottom][j])
+                j -= 1
+            bottom -= 1
+            
+            
+            j = bottom
+            while j >= top and len(answer) < rows * cols:
+                answer.append(matrix[j][left])
+                j -= 1
+            left += 1
+            
 
+        return answer        
