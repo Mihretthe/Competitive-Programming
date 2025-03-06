8198dbf0d5e3db8fc0c12b17ff102e7782c025ac
Mihret Tekalgn <115806991+Mihretthe@users.noreply.github.com>
2025-03-06 12:01:52 +0300

    Time: 169 ms (27.01%), Space: 78.7 MB (27.43%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,26 +1,95 @@
+class ListNode:
+    def __init__(self, key = -1, val = -1):
+        self.key = key
+        self.val = val
+        self.next = None
+    
+    def __str__(self):
+        temp = self.next
+        ans = [f"({self.key},{self.val})"]
+        while temp:
+            ans.append(f"({temp.key},{temp.val})")
+            temp = temp.next
+        
+        return " -> ".join(ans)
+
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.store = {}
-        self.recently = []
         self.capacity = capacity
+        self.dummy = ListNode()
+        self.hashmap = {}
+        self.tail = ListNode()
+        self.dummy.next = self.tail
+        self.hashmap['tail'] = [self.tail, self.dummy]
+        
 
     def get(self, key: int) -> int:
-        if key in self.store.keys():
-            if key in self.recently:
-                self.recently.remove(key)
-            self.recently.append(key)
-            return self.store[key]
+        if key in self.hashmap:
+            node, prev = self.hashmap[key]
+            prev.next = prev.next.next
+            temp = prev.next
+            if temp == self.tail:
+                self.hashmap['tail'][1] = prev 
+            else:
+                self.hashmap[temp.key][1] = prev 
+            
+            previous = self.hashmap['tail'][1]
+            node.next = None
+            previous.next = node
+            node.next = self.tail
+            self.hashmap['tail'][1] = node
+            self.hashmap[key] = [node, previous]
+
+           
+            return node.val
+        
         return -1
+        
 
     def put(self, key: int, value: int) -> None:
-        if key not in self.store.keys() and len(self.store) >= self.capacity:
-            del self.store[self.recently[0]]
-            self.recently = self.recently[1:]
-        self.store[key] = value
-        if key in self.recently:
-            self.recently.remove(key)
-        self.recently.append(key)
+
+        if key in self.hashmap:
+            node, prev = self.hashmap[key]
+            node.val = value
+            prev.next = prev.next.next
+            temp = prev.next
+            if temp == self.tail:
+                self.hashmap['tail'][1] = prev 
+            else:
+                self.hashmap[temp.key][1] = prev 
+            
+            previous = self.hashmap['tail'][1]
+            node.next = None
+            previous.next = node
+            node.next = self.tail
+            self.hashmap['tail'][1] = node
+            self.hashmap[key] = [node, previous]
+
+            
+            return node.val
+            
+        else:
+            if len(self.hashmap) - 1 == self.capacity:
+                node = self.dummy.next
+                nextt = node.next
+                if nextt == self.tail:
+                    self.hashmap['tail'][1] = self.dummy
+                else:
+                    self.hashmap[nextt.key][1] = self.dummy
+                del self.hashmap[node.key]
+                self.dummy.next = self.dummy.next.next
+
+            new_node = ListNode(key, value)
+            previous = self.hashmap['tail'][1]
+            new_node.next = None
+            previous.next = new_node
+            new_node.next = self.tail
+            self.hashmap['tail'][1] = new_node
+            self.hashmap[key] = [new_node, previous]
+
+   
+        
 
 
 # Your LRUCache object will be instantiated and called as such:
