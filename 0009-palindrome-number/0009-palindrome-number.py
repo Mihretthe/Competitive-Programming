452ddd78c8d5f1d786dddcf8b132f3dd4ef53e96
Mihret Tekalgn <115806991+Mihretthe@users.noreply.github.com>
2025-03-31 10:02:42 +0300

    Time: 8 ms (52.93%), Space: 17.9 MB (39.34%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,8 +1,20 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        s=str(x)
-        v=s[::-1]
-        if s==v:
-            return True
-        else:
-            return False
+        # to check if the number is palindrome 
+        #  1. change the number to a string : O(log(abs(number))) in base 10
+        #  2. check if it is the same forward and backward by using two pointers from the left most and right most
+
+        number = str(x)
+        
+        left = 0
+        right = len(number) - 1
+
+        while left < right:
+            if number[left] != number[right]:
+                return False
+            
+            # move the colliding pointers
+            left += 1
+            right -= 1
+
+        return True
